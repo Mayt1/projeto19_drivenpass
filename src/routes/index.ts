31f9878dd1a2ba115dc9d195400647f1d9c7d1a5
VictorUlian <yctor.ulian@gmail.com>
2022-07-28 08:49:46 -0300

    fix: fix the credential router imports

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import authRouter from "./authRouter";
 import cardRouter from "./cardRouter";
+import credentialRouter from "./credentialRouter";
 
 
 const router = Router();
 
 router.use(authRouter);
 router.use(cardRouter);
+router.use(credentialRouter);
 
 
 export default router;
